84fe47f5d32c68607c5d058e7b4b783b984d473d
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2020-06-03 22:33:14 -0300

    ajeitando .gitignore

---
+++ tests/token.py
@@ -0,0 +1,4 @@
+token_comum = {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTExOTA5NjAsInN1YiI6MiwiYWRtaW4iOmZhbHNlLCJuYW1lIjoiY29tdW0ifQ.TnHhMKuSO9YQ63r3_AVaCbQCOFG90xVcN8wFNB1N-kc"}
+token_admin = headerAdmin = {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTExOTEwMjYsInN1YiI6MSwiYWRtaW4iOnRydWUsIm5hbWUiOiJhZG1pbiJ9.fyCdC-INvzrNAAQE-aRH1LfpRR-8ZjjlQ9Gnw6AkZKc"}
+user='dho619'
+password='619dho'
